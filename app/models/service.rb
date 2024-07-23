5381a0d5ca831bddd68bf764858c6722c06624d6
Mason Roberts <mrobe18@gmail.com>
2024-07-23 16:48:57 -0400

    repaired notice and alert on root. Locked down categories, created issue 6. Views and partials worked, lot of redundent/wet code.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,11 @@
 class Service < ApplicationRecord
+  # Service must be in a category
   validates :service_category_id, presence: true
+  # Service belongs to a category
   belongs_to :service_category
 
+  # Descriptions can be rich text
   has_rich_text :description
+  # Service can have an image
   has_one_attached :image, dependent: :destroy
 end
